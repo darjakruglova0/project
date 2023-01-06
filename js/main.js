52622918fea8fb23a5def598030b84cf68f6a6c9
Daria Kachur <darjakruglova0@gmail.com>
2023-01-06 15:31:04 +0300

    06.01

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 "use strict";
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+console.log(numberOfFilms);
+
+
+const personalMovieDB ={
+     count:numberOfFilms,
+     movies:{},
+     actors:{},
+     genres:[],
+     privat:false
+};
+const watchFilms = prompt('Один из последних просмотренных фильмов?',''),
+      watchFilms2 = prompt('Насколько оцените его?',''),
+      watchFilms3 = prompt('Один из последних просмотренных фильмов?',''),
+      watchFilms4 = prompt('Насколько оцените его?','');
+
+personalMovieDB.movies[watchFilms]= watchFilms2;
+personalMovieDB.movies[watchFilms3]= watchFilms4;
+
+console.log(personalMovieDB);
